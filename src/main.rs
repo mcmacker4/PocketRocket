b0af0d21223b24fce33247ba8bd29e9eabc9f015
Hans Geel <mcmacker4@gmail.com>
2023-11-30 18:12:16 +0100

    Skeleton

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use rocket::{get, launch, routes};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello World"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
