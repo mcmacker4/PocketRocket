912a7408495f39f082fa37dfb2ab99f493179c31
cout970 <thecout970@gmail.com>
2023-12-01 13:03:39 +0100

    Add scripts to start a local docker container with mariadb

---
+++ scripts/start_db_shell.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if ! command -v 'docker' &> /dev/null
+then
+    echo "Error: docker not found, please install"
+    exit 1
+fi
+
+# -it for interactive
+# --rm to remove container after exit
+# local-mariadb name of the container
+# mariadb:latest to use latest image
+# Database credentials and settings for debug environment
+docker run -it --rm --network="host" mariadb:latest \
+  mariadb --host 127.0.0.1 --port 3306 --user root --password=root
---
+++ scripts/start_local_db.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+if ! command -v 'docker' &> /dev/null
+then
+    echo "Error: docker not found, please install"
+    exit 1
+fi
+
+if docker ps | grep -q 'local-mariadb'; then
+    echo "Abort: local-mariadb container already running"
+    exit 1
+fi
+
+if docker ps -all | grep -q 'local-mariadb'; then
+    echo "Existing container found, but its stopped, removing"
+    docker remove local-mariadb
+fi
+
+# Start local mariadb container
+# --detach: Run container in background
+# --name: Assign a name to the container
+# --network: Use a internal network with filtered ports
+# --restart: Restart the container unless it is explicitly stopped
+# --log-driver: Log to a local file with a maximum size to avoid filling up the disk
+# --env: Default root password for mariadb in the development environment
+# -p: Expose port 3306 to allow access from host
+# Use the latest mariadb image
+docker run \
+  --detach \
+  --name local-mariadb \
+  --network="bridge" \
+  --restart=unless-stopped \
+  --log-driver local --log-opt max-size=10m \
+  --env MARIADB_ROOT_PASSWORD=root \
+  --env MARIADB_DATABASE=test \
+  -p 3306:3306 \
+  mariadb:latest
+
+echo "Done"
